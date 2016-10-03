2bdc04e0c7ef2c0a064ee5935221db3e5c063b4d
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-10-03 15:10:39 +0300

    A couple of small fixes: crawl starts properly now

--- hh_deep_deep/crawl.py
+++ hh_deep_deep/crawl.py
@@ -3,6 +3,7 @@ import hashlib
 import logging
 from pathlib import Path
 import subprocess
+import time
 from typing import Dict, List
 
 
@@ -15,9 +16,12 @@ class CrawlProcess:
         self.id_ = id_
         self.seeds = seeds
         self.page_clf_data = page_clf_data
-        self.root = (self.jobs_root /
-                     hashlib.md5(id_.encode('utf8')).hexdigest()
-                     ).absolute()
+        self.root = (
+            self.jobs_root /
+            '{}_{}'.format(
+                int(time.time()),
+                hashlib.md5(id_.encode('utf8')).hexdigest()[:12]
+            )).absolute()
         self.deep_deep_image = deep_deep_image or 'deep-deep'
 
     @classmethod
@@ -32,13 +36,13 @@ class CrawlProcess:
         self.root.joinpath('id.txt').write_text(self.id_)
         self.root.joinpath('page_clf.joblib').write_bytes(self.page_clf_data)
         with self.root.joinpath('seeds.csv').open('wt') as f:
-            csv.writer(f).writerows(self.seeds)
+            csv.writer(f).writerows([url] for url in self.seeds)
         args = [
             'docker', 'run', '-d',
             '-v', '{}:{}'.format(self.root, '/job'),
             self.deep_deep_image,
             'scrapy', 'crawl', 'relevant',
-            '-a', 'seeds_url=/job/seeds.txt',
+            '-a', 'seeds_url=/job/seeds.csv',
             '-a', 'checkpoint_path=/job',
             '-a', 'classifier_path=/job/page_clf.joblib',
             '-o', 'gzip:/job/items.jl',
@@ -47,7 +51,7 @@ class CrawlProcess:
             '-s', 'CLOSESPIDER_ITEMCOUNT=1000000',
         ]
         logging.info('Starting crawl in {}'.format(self.root))
-        self.pid = subprocess.check_output(args).decode('utf8')
+        self.pid = subprocess.check_output(args).decode('utf8').strip()
         logging.info('Crawl started, container id {}'.format(self.pid))
         self.root.joinpath('pid.txt').write_text(self.pid)
 
